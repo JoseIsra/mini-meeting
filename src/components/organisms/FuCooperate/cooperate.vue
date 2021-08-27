9292b75b60e6cef4766fa2086ac3f7b432d73b01
JoseIsra <israelmyname@gmail.com>
2021-08-27 15:44:09 -0500

    preparing main page

--- src/components/organisms/FuCooperate/cooperate.vue
+++ src/components/organisms/FuCooperate/cooperate.vue
@@ -1,14 +1,24 @@
 <template>
   <section class="o-cooperate">
-    <header>
-      <figure>
+    <header class="o-cooperate__header">
+      <figure class="o-cooperate__header__logo">
         <img
-          src="https://encrypted.fractalup.com/file/MainPublic/fractalup_assets/logo/FractalUp-Collaborate.svg"
+          class="o-cooperate__header__logo__image"
+          src="https://encrypted.fractalup.com/file/MainPublic/fractalup_assets/logo/LogoFU_head.svg"
           alt="logo-collaborate"
         />
       </figure>
     </header>
-    <main class="o-cooperate__menuBar">iconos aqui</main>
+    <main class="o-cooperate__screen">
+      <figure class="o-cooperate__screen__imgBox">
+        <img
+          class="o-cooperate__screen__imgBox__image"
+          alt="fractalitos"
+          src="https://encrypted.fractalup.com/file/MainPublic/fractalup_assets/landing/main.png"
+        />
+      </figure>
+    </main>
+    <div class="o-cooperate__menuBar">iconos aqui</div>
   </section>
 </template>
 

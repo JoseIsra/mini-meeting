333500ae982698fbb304239f253fb124fc93bd23
dialguiba <dialguiba@gmail.com>
2021-08-27 16:02:50 -0500

    refactor(project config): add ts to eslint extension

--- quasar.conf.js
+++ quasar.conf.js
@@ -93,7 +93,7 @@ module.exports = configure(function (ctx) {
       chainWebpack(chain) {
         chain
           .plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]);
+          .use(ESLintPlugin, [{ extensions: ['js', 'vue', 'ts'] }]);
       },
     },
 

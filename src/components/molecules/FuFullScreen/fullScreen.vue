0b0de501273bb1df5abadf435f66f799727ddb13
JoseIsra <israelmyname@gmail.com>
2021-09-23 12:39:24 -0500

    implemented full screen component

---
+++ src/components/molecules/FuFullScreen/fullScreen.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="m-full">
+    <fu-full-screen-user v-if="fullScreenMode === 'user'" />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FuFullScreenUser from 'molecules/FuFullScreenUser';
+import { useToogleFunctions } from '@/composables';
+
+export default defineComponent({
+  name: 'FuFullScreen',
+  components: { FuFullScreenUser },
+  setup() {
+    const { fullScreenMode } = useToogleFunctions();
+    return {
+      fullScreenMode,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import './fullScreen.scss';
+</style>

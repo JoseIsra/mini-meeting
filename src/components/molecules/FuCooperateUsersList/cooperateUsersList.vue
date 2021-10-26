7043cb330a8e8e443b785ee4d3b15f17daa59c5e
JoseIsra <israelmyname@gmail.com>
2021-10-26 12:31:40 -0500

    preparando cambio de mano

--- src/components/molecules/FuCooperateUsersList/cooperateUsersList.vue
+++ src/components/molecules/FuCooperateUsersList/cooperateUsersList.vue
@@ -133,7 +133,7 @@
             v-if="notificateHandUp(userMe.id)"
             name="front_hand"
             class="m-list__content__userBox__avatar__handIcon"
-            size="25px"
+            size="20px"
           />
           <q-img
             v-else
@@ -311,7 +311,7 @@
           <q-icon
             v-if="notificateHandUp(participant.id)"
             name="front_hand"
-            size="25px"
+            size="20px"
             class="m-list__content__userBox__avatar__handIcon"
           />
           <q-img

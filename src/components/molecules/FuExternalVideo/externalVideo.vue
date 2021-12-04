b8e7b41f2fbb808a199565b4576f7d31e94ecde5
JoseIsra <israelmyname@gmail.com>
2021-12-04 09:45:14 -0500

    :tools: refactorizando videojs compartido

--- src/components/molecules/FuExternalVideo/externalVideo.vue
+++ src/components/molecules/FuExternalVideo/externalVideo.vue
@@ -4,7 +4,6 @@
     :style="redimensionVideoSize"
   >
     <q-btn
-      :style="[simpleMortal ? { visibility: 'hidden' } : '']"
       icon="play_arrow"
       v-show="showPlayButton"
       class="m-video__btn --playVideo"
@@ -12,11 +11,8 @@
     />
     <video
       id="specialId"
-      :class="[
-        { 'vjs-poster': posterClass },
-        { 'vjs-youtube': posterClass },
-        { 'vjs-tech': simpleMortal },
-      ]"
+      :class="[{ 'vjs-poster': posterClass }, { 'vjs-youtube': posterClass }]"
+      autoplay
       ref="videoPlayer"
       class="video-js"
     ></video>
@@ -60,11 +56,11 @@ export default defineComponent({
     const player = ref<videojs.Player>({} as videojs.Player);
     const showPlayButton = ref(false);
     const canManipulateVideo = ref(userMe.roleId === 0);
-    const simpleMortal = ref(userMe.roleId == 1);
     const { screenMinimized } = useScreen();
     const optionsForPlayer = reactive<videojs.PlayerOptions>({
-      controls: canManipulateVideo.value || screenMinimized.value,
+      controls: !screenMinimized.value,
       bigPlayButton: false,
+      autoplay: true,
       responsive: true,
       controlBar: {
         progressControl: {
@@ -107,17 +103,18 @@ export default defineComponent({
           });
           player.value.on('ready', () => {
             posterClass.value = true;
-            console.log('go on video go on🤭', posterClass.value);
             void player.value.play();
           });
           player.value.controlBar.on('mouseup', () => {
-            setTimeout(() => {
-              sendData(roomState.hostId, {
-                remoteInstance: videoPlayer.value,
-                videoCurrentTime: calculateCurrentSelectedTime.value,
-                eventType: 'UPDATE_VIDEOTIME',
-              });
-            }, 500);
+            if (canManipulateVideo.value) {
+              setTimeout(() => {
+                sendData(roomState.hostId, {
+                  remoteInstance: videoPlayer.value,
+                  videoCurrentTime: calculateCurrentSelectedTime.value,
+                  eventType: 'UPDATE_VIDEOTIME',
+                });
+              }, 500);
+            }
           });
         }
       );
@@ -131,10 +128,13 @@ export default defineComponent({
 
       showPlayButton.value = false;
       void player.value.play();
-      sendData(roomState.hostId, {
-        remoteInstance: videoPlayer.value,
-        eventType: 'PLAYING_VIDEO',
-      });
+
+      if (canManipulateVideo.value) {
+        sendData(roomState.hostId, {
+          remoteInstance: videoPlayer.value,
+          eventType: 'PLAYING_VIDEO',
+        });
+      }
     };
 
     const handlePause = () => {
@@ -143,10 +143,13 @@ export default defineComponent({
         isVideoPlaying: false,
       });
       showPlayButton.value = true;
-      sendData(roomState.hostId, {
-        remoteInstance: videoPlayer.value,
-        eventType: 'PAUSE_VIDEO',
-      });
+
+      if (canManipulateVideo.value) {
+        sendData(roomState.hostId, {
+          remoteInstance: videoPlayer.value,
+          eventType: 'PAUSE_VIDEO',
+        });
+      }
     };
 
     onBeforeUnmount(() => {
@@ -193,7 +196,6 @@ export default defineComponent({
       handlePlaying,
       showPlayButton,
       userMe,
-      simpleMortal,
       watchMinimized,
       posterClass,
       redimensionVideoSize,
@@ -202,9 +204,9 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped>
-// .vjs-youtube .vjs-poster {
-//   display: none !important;
-// }
+<style lang="scss">
+.vjs-youtube .vjs-poster {
+  display: none !important;
+}
 @import './externalVideo.scss';
 </style>

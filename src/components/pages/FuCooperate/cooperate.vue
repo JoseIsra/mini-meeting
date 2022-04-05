e2b5ef8898d9f2514155ad2aa4fae8f3e30dedd5
JoseIsra <israelmyname@gmail.com>
2022-04-05 14:23:08 -0500

    🔥 (route): change main route
    route now pointing cooperateitself

--- src/components/pages/FuCooperate/cooperate.vue
+++ src/components/pages/FuCooperate/cooperate.vue
@@ -8,6 +8,8 @@
 import { defineComponent } from 'vue';
 
 import FuTCooperate from 'templates/FuCooperate';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import FractalUpCooperateZoid from '@/utils/zoid';
 
 export default defineComponent({
   name: 'FuPCooperate',
--- src/components/pages/FuPrejoinPage/prejoinPage.vue
+++ src/components/pages/FuPrejoinPage/prejoinPage.vue
@@ -7,8 +7,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TFuPrejoinPage from 'templates/FuPrejoinPage';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import FractalUpCooperateZoid from '@/utils/zoid';
 
 export default defineComponent({
   name: 'FuPrejoinPage',

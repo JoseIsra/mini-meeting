28a511e30ba989ccf6455f6d6c05e00b53642924
JoseIsra <israelmyname@gmail.com>
2022-04-05 12:31:09 -0500

    🚀 (only prejoin): Prejoin page only
    testing routes problem with zoid

--- src/components/layouts/FuMainLayout/mainLayout.vue
+++ src/components/layouts/FuMainLayout/mainLayout.vue
@@ -4,8 +4,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import FractalUpCooperateZoid from '@/utils/zoid';
 export default defineComponent({
   name: 'FuMainLayout',
   setup() {
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,23 +4,24 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
-    component: () => import('layouts/FuMainLayout'),
-    redirect: {
-      name: 'prejoin',
-    },
-    children: [
-      {
-        path: 'prejoin',
-        name: 'prejoin',
-        component: () => import('pages/FuPrejoinPage'),
-      },
-      {
-        path: 'meet',
-        name: 'meet',
-        component: () => import('pages/FuCooperate'),
-      },
-    ],
+    // component: () => import('layouts/FuMainLayout'),
+    component: () => import('pages/FuPrejoinPage'),
   },
+  //   redirect: {
+  //     name: 'prejoin',
+  //   },
+  //   children: [
+  //     {
+  //       path: 'prejoin',
+  //       name: 'prejoin',
+  //     },
+  //     {
+  //       path: 'meet',
+  //       name: 'meet',
+  //       component: () => import('pages/FuCooperate'),
+  //     },
+  //   ],
+  // },
 
   // Always leave this as last one,
   // but you can also remove it

ef9f7f3d7e35a4537252ce022c584158e32d406f
JoseIsra <israelmyname@gmail.com>
2022-04-05 12:21:24 -0500

    ♻️ (redirect): zoid not working
    Zoid is not working properly

--- src/components/layouts/FuMainLayout/mainLayout.vue
+++ src/components/layouts/FuMainLayout/mainLayout.vue
@@ -4,7 +4,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import FractalUpCooperateZoid from '@/utils/zoid';
 export default defineComponent({
   name: 'FuMainLayout',
   setup() {
